ada9f7889bfdebaaa562cacb739cff6739c387a1
Alex Gavrylyuk <alexga@ip-com.co.il>
2026-01-08 16:34:10 +0200

    added babel, added extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,18 @@ import webpack from 'webpack';
 import { BuildOptions } from './types/config';
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: [['i18next-extract', { locales: ['en', 'ru'], keyAsDefaultValue: true }]],
+      },
+    },
+  };
+
   const typescriptLoader = {
     test: /\.tsx?$/,
     use: 'ts-loader',
@@ -39,5 +51,6 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
       },
     ],
   };
-  return [typescriptLoader, cssLoader, svgLoader, fileLoader];
+
+  return [babelLoader, typescriptLoader, cssLoader, svgLoader, fileLoader];
 }
